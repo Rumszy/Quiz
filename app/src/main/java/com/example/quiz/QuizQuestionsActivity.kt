e52d86b995882c8ae3ee76c20c9168e10bf5ee25
Rumszy <macsku1999@gmail.com>
2023-05-02 13:34:45 +0200

    Finalizing the UI.

--- app/src/main/java/com/example/quiz/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quiz/QuizQuestionsActivity.kt
@@ -1,40 +1,90 @@
 package com.example.quiz
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
+    private var tvQuestion: TextView? = null
+    private var ivFlag: ImageView? = null
+    private var pbProgress: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvOptionOne: TextView? = null
+    private var tvOptionTwo: TextView? = null
+    private var tvOptionThree: TextView? = null
+    private var tvOptionFour: TextView? = null
+
+    var i = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
         //Log.i("The size of the question list is: ", "${Constants.getQuestions().size}")
 
-        val tvQuestion: TextView = findViewById(R.id.tv_question)
-        val ivFlag: ImageView = findViewById(R.id.iv_flag)
-        val pbProgress: ProgressBar = findViewById(R.id.pb_progress)
-        val tvProgress: TextView = findViewById(R.id.tv_progress)
-        val tvOptionOne: TextView = findViewById(R.id.tv_optionOne)
-        val tvOptionTwo: TextView = findViewById(R.id.tv_optionTwo)
-        val tvOptionThree: TextView = findViewById(R.id.tv_optionThree)
-        val tvOptionFour: TextView = findViewById(R.id.tv_optionFour)
+        tvQuestion = findViewById(R.id.tv_question)
+        ivFlag = findViewById(R.id.iv_flag)
+        pbProgress = findViewById(R.id.pb_progress)
+        tvProgress = findViewById(R.id.tv_progress)
+        tvOptionOne = findViewById(R.id.tv_optionOne)
+        tvOptionTwo = findViewById(R.id.tv_optionTwo)
+        tvOptionThree = findViewById(R.id.tv_optionThree)
+        tvOptionFour = findViewById(R.id.tv_optionFour)
 
         val questions = Constants.getQuestions()
 
-        var i = 0
+        var currentposition = 1
 
-            tvQuestion.text = questions[i].question
-            ivFlag.setImageResource(questions[0].image)
-            pbProgress.progress = (questions[i].id)
-            tvProgress.text = "${questions[i].id} / ${pbProgress.max}"
-            tvOptionOne.text = questions[i].optionOne
-            tvOptionTwo.text = questions[i].optionTwo
-            tvOptionThree.text = questions[i].optionThree
-            tvOptionFour.text = questions[i].optionFour
+        var question : Question = questions[currentposition - 1]
 
+        tvQuestion?.text = question.question
+        ivFlag?.setImageResource(question.image)
+        pbProgress?.progress = (question.id)
+        tvProgress?.text = "${question.id} / ${pbProgress?.max}"
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
+    }
+
+    fun onOption(view: View) {
+
+        var options = ArrayList<TextView?>()
+
+        options.add(tvOptionOne)
+        options.add(tvOptionTwo)
+        options.add(tvOptionThree)
+        options.add(tvOptionFour)
+
+
+        for (i in options) {
+            if (i?.text == (view as TextView).text) {
+                i?.setTypeface(null, Typeface.BOLD)
+            } else {
+                i?.setTypeface(null, Typeface.NORMAL)
+            }
+        }
+
+//        when ((view as TextView).text) {
+//            "tv_optionOne" -> { tvOptionOne.setTypeface(null, Typeface.BOLD)
+//                tvOptionTwo.setTypeface(null, Typeface.NORMAL)
+//                tvOptionThree.setTypeface(null, Typeface.NORMAL)
+//                tvOptionFour.setTypeface(null, Typeface.NORMAL)
+//            }
+//        }
+//        (view as TextView).setTypeface(null, Typeface.BOLD)
+    }
+
+    fun onSubmit(view: View) {
+        i++
+        onUpdate()
+    }
+
+    private fun onUpdate() {
 
     }
 }
